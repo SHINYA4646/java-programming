a92d86e7c9d7d410883682bb15cebd6330ad61b1
SHINYA4646 <43059425+SHINYA4646@users.noreply.github.com>
2018-10-03 12:38:57 +0530

    Add files via upload

---
+++ positive or negative.java
@@ -0,0 +1,15 @@
+class Main{
+public static void main(String[]args){
+  int i=3;
+  if(i>0)
+  {
+    System.out.println(i+"positive");
+  }
+    else
+    if(i<0)
+    {
+    System.out.println(i+"negative");
+  }
+}
+}
+	
