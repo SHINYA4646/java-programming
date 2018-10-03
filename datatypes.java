2133bda726ffb576a9ebca5364e6127e1a8e0879
SHINYA4646 <43059425+SHINYA4646@users.noreply.github.com>
2018-10-03 12:20:45 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	string name="shiny";
+    string city="nagercoil";
+    byte age="23";
+      float height="5.3f";
+      boolean married="false";
+      char sex="female";
+      double city population="45637";
+      System.out.print("name:"+name);
+      System.out.print("city:"+city);
+      System.out.print("age":+age);
+      System.out.ptint("height:"+height);
+      System.out.print("married:"+married);
+      System.out.print("sex:"+sex);
+      System.out.print("city population:"+city population);
+	}
+}
