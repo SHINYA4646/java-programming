e1b59d2efe9455481e458dd149e6c5f6d9409324
SHINYA4646 <43059425+SHINYA4646@users.noreply.github.com>
2018-10-04 12:05:11 +0530

    Add files via upload

---
+++ odd even.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+  Scanner reader = new Scanner(System.in);
+ System.out.print("Enter a number: ");
+   int num = reader.nextInt();
+   if(num % 2 == 0)
+   System.out.println( "  even");
+      else
+         System.out.println("  odd");
+    }
+}
