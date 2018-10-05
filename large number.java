726ccdc50841c0e5666ae9b1ea999b02625ee19f
SHINYA4646 <43059425+SHINYA4646@users.noreply.github.com>
2018-10-05 11:41:11 +0530

    Add files via upload

---
+++ large number.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+double a=2.3,b=4.5,c=5.2;
+if(a>b && a>c)
+    System.out.println(" is large"+a);
+    else if(b>a && b>c)
+    System.out.println("is large"+b);
+    else
+System.out.println("c is large"+c);
+
+  } 
+} 
